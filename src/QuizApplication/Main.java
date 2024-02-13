0361eca8d6360de9e0756ed4e2847fdb273ee46d
Jeba1707 <jebapc2023@gmail.com>
2024-02-13 20:27:13 +0600

    save ques to file and load ques to file ..done

--- src/QuizApplication/Main.java
+++ src/QuizApplication/Main.java
@@ -1,8 +1,16 @@
 package QuizApplication;
 
+import java.util.Scanner;
+
 public class Main {
+    private static String mathfile = "E:\\Study\\JavaProjects\\Java Collection Framework Projects\\src\\QuizApplication\\math.ser";
+    private static String gkfile = "E:\\Study\\JavaProjects\\Java Collection Framework Projects\\src\\QuizApplication\\gk.ser";
+    private static String foodfile = "E:\\Study\\JavaProjects\\Java Collection Framework Projects\\src\\QuizApplication\\food.ser";
+    private static String historyfile = "E:\\Study\\JavaProjects\\Java Collection Framework Projects\\src\\QuizApplication\\history.ser";
+   private static QuizApp quizApp = new QuizApp(mathfile,gkfile,foodfile,historyfile);
+   private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        QuizApp quizApp = new QuizApp();
+
 
 
         //creating and Adding GkQues
@@ -12,6 +20,8 @@ public class Main {
         Question gk4 = new GKquestion("True or False: The Earth is flat.","False");
         Question gk5 = new GKquestion(  "What is the chemical symbol for water?","A. H2O", "B. CO2", "C. O2","A" );
 
+       // quizApp.addinList(gk1); quizApp.addinList(gk2); quizApp.addinList(gk3); quizApp.addinList(gk4); quizApp.addinList(gk5);
+
 
         Question food1 = new Foodquestion("What is the most consumed fruit in the world?","A.Banana", "B. Mango", "C.Orange","A");
         Question food2 = new Foodquestion(  "Which country is responsible for creating hot sauce?","A. China", "B. Mexico", "C. Japan","B");
@@ -19,7 +29,7 @@ public class Main {
         Question food4 = new Foodquestion("True or False: Gelatin is the base ingredient of gummy bears.","True");
         Question food5 = new Foodquestion(  "Rice is mainly composed of which nutrient?","A. Fat", "B. carbohydrate", "C. protein","B" );
 
-
+       // quizApp.addinList(food1); quizApp.addinList(food2); quizApp.addinList(food3); quizApp.addinList(food4); quizApp.addinList(food5);
 
         Question his1 = new Historyquestion("What year did Bangladesh gain independence from Pakistan?","A.1965", "B. 1971", "C.1952","B");
         Question his2 = new Historyquestion(  "Who is known as the Father of the Nation in Bangladesh?","A.  Sheikh Mujibur Rahman", "B. Ziaur Rahman", "C.  A. K. Fazlul Huq","A");
@@ -27,7 +37,7 @@ public class Main {
         Question his4 = new Historyquestion("True or False: Bangladesh was formerly known as East Pakistan.","True");
         Question his5 = new Historyquestion(  "True or False: The capital of Bangladesh, Dhaka, was indeed known as Jahangir Nagar during the Mughal period.","True" );
 
-
+      // quizApp.addinList(his1); quizApp.addinList(his2); quizApp.addinList(his3); quizApp.addinList(his4); quizApp.addinList(his5);
 
         //creating and Adding Mathques
         Question math1 = new Mathquetion("What is the sum of 130+125+191?","A. 335", "B. 456", "C. 446","C");
@@ -36,6 +46,17 @@ public class Main {
         Question math4 = new Mathquetion("True or False: The product of 121 x 0 x 200 x 25 is 0","True");
         Question math5 = new Mathquetion(  "True or False: -10 is a whole number.","False" );
 
+       // quizApp.addinList(math1); quizApp.addinList(math2); quizApp.addinList(math3); quizApp.addinList(math4); quizApp.addinList(math5);
+
+//        //quizApp.viewQuetions();
+//        quizApp.viewGKQuetions();
+//        quizApp.viewMathQuetions();
+   // quizApp.viewGKQuetions();
+
+        quizApp.viewHistoryQuetions();
+        quizApp.viewMathQuetions();
+        quizApp.viewFoodQuetions();
+        quizApp.viewGKQuetions();
 
 
 
--- src/QuizApplication/QuizApp.java
+++ src/QuizApplication/QuizApp.java
@@ -1,36 +1,177 @@
 package QuizApplication;
 
+import ContactManagementSystem.Contact;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
 
 public class QuizApp {
-
+    private  List<Question> allQuestion ;
     private List<Question> gKquestions;
     private List<Question> historyquestions;
     private List<Question> mathquetions;
     private List<Question> foodquestions;
 
-    public QuizApp() {
+    private String mathfile ;   private String gkfile ;   private String foodfile ;   private String historyfile ;
+
+
+    public QuizApp(String mathfile,String gkfile,String foodfile,String historyfile) {
+        this.allQuestion=new ArrayList<>();
         this.gKquestions = new ArrayList<>();
         this.historyquestions = new ArrayList<>();
         this.mathquetions = new ArrayList<>();
         this.foodquestions = new ArrayList<>();
+        this.mathfile=mathfile;
+        this.gkfile=gkfile;
+        this.foodfile=foodfile;
+        this.historyfile=historyfile;
+
     }
 
-    public void addQuestion(Question question){
-
+    public void addinList(Question question){
+      //  allQuestion.add(question);
         if(question instanceof GKquestion){
             gKquestions.add(question);
+            saveGKQuesToFile();
         } else if (question instanceof Historyquestion) {
             historyquestions.add(question);
+            saveHistoryQuesToFile();
         }else if (question instanceof Mathquetion) {
             mathquetions.add(question);
+            saveMathQuesToFile();
         }else if (question instanceof Foodquestion) {
             foodquestions.add(question);
+            saveFoodQuesToFile();
         }
+
+       System.out.println("Question added successfully.");
 
     }
 
+    public void viewGKQuetions() {
+        System.out.println("GKQues");
+       loadGKQuesFromFile();
 
+        if (gKquestions.isEmpty()) {
+            System.out.println("No question available.");
+        } else {
+            System.out.println("Questions:");
+            for (Question question : gKquestions) {
+                System.out.println(question.getQuestion());
+            }
+        }
+    }
+    public void viewFoodQuetions() {
+        System.out.println("FoodQues");
+       loadFoodQuesFromFile();
+
+        if (foodquestions.isEmpty()) {
+            System.out.println("No question available.");
+        } else {
+            System.out.println("Questions:");
+            for (Question question : foodquestions) {
+                System.out.println(question.getQuestion());
+            }
+        }
+    }public void viewMathQuetions() {
+        System.out.println("MathQues");
+        loadMathQuesFromFile();
+
+        if (mathquetions.isEmpty()) {
+            System.out.println("No question available.");
+        } else {
+            System.out.println("Questions:");
+            for (Question question : mathquetions) {
+                System.out.println(question.getQuestion());
+            }
+        }
+    }public void viewHistoryQuetions() {
+        System.out.println("HisQues");
+       loadHistoryQuesFromFile();
+
+        if (historyquestions.isEmpty()) {
+            System.out.println("No question available.");
+        } else {
+            System.out.println("Questions:");
+            for (Question question : historyquestions) {
+                System.out.println(question.getQuestion());
+            }
+        }
+    }
+
+    private void saveMathQuesToFile() {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(mathfile))) {
+            outputStream.writeObject(mathquetions);
+        } catch (IOException e) {
+            System.out.println("Error saving questions to file: " + e.getMessage());
+        }
+    }
+    private void saveGKQuesToFile() {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(gkfile))) {
+            outputStream.writeObject(gKquestions);
+        } catch (IOException e) {
+            System.out.println("Error saving questions to file: " + e.getMessage());
+        }
+    }
+    private void saveFoodQuesToFile() {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(foodfile))) {
+            outputStream.writeObject(foodquestions);
+        } catch (IOException e) {
+            System.out.println("Error saving questions to file: " + e.getMessage());
+        }
+    }
+    private void saveHistoryQuesToFile() {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(historyfile))) {
+            outputStream.writeObject(historyquestions);
+        } catch (IOException e) {
+            System.out.println("Error saving questions to file: " + e.getMessage());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void loadGKQuesFromFile() {
+        File file = new File(gkfile);
+        if (file.exists()) {
+            try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(gkfile))) {
+                gKquestions = (List<Question>) inputStream.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("Error loading Question from file: " + e.getMessage());
+            }
+        }
+    }
+
+    private void loadMathQuesFromFile() {
+        File file = new File(mathfile);
+        if (file.exists()) {
+            try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(mathfile))) {
+                mathquetions = (List<Question>) inputStream.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("Error loading Question from file: " + e.getMessage());
+            }
+        }
+    }
+    private void loadFoodQuesFromFile() {
+        File file = new File(foodfile);
+        if (file.exists()) {
+            try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(foodfile))) {
+                foodquestions = (List<Question>) inputStream.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("Error loading Question from file: " + e.getMessage());
+            }
+        }
+    }
+    private void loadHistoryQuesFromFile() {
+        File file = new File(historyfile);
+        if (file.exists()) {
+            try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(historyfile))) {
+                historyquestions = (List<Question>) inputStream.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                System.out.println("Error loading Question from file: " + e.getMessage());
+            }
+        }
+    }
 }
+
+
